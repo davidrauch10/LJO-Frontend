7dd0a99f8b5458b0ab036f878ae37948e3cf857f
David Rauch <david.rauch10@gmail.com>
2022-05-05 19:15:23 +0200

    Stand 05. Mai 2022

--- src/app/helfer-liste/helfer-liste.component.ts
+++ src/app/helfer-liste/helfer-liste.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Helfer } from '../models/helfer.models';
+import { HelferService } from '../services/helfer.service';
 
 @Component({
   selector: 'app-helfer-liste',
@@ -7,21 +9,25 @@ import { Helfer } from '../models/helfer.models';
   styleUrls: ['./helfer-liste.component.css']
 })
 export class HelferListeComponent implements OnInit {
-  public helfer: Helfer[] = [
-    {
-      id: 1,
-      name: "Fabian Zimmermann",
-      adress: "Duenserberg Alte Landstrasse 1",
-      phonenumber: "0664 9160879",
-    },
-    {
-      id: 2,
-      name: "Simon Kremmel",
-      adress: "Raggal 168",
-      phonenumber: "0664 8243948"
-    },
-  ];
-  public constructor() { }
+  // public helfer: Helfer[] = [
+  //   {
+  //     id: 1,
+  //     name: "Fabian Zimmermann",
+  //     adress: "Duenserberg Alte Landstrasse 1",
+  //     phonenumber: "0664 9160879",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Simon Kremmel",
+  //     adress: "Raggal 168",
+  //     phonenumber: "0664 8243948"
+  //   },
+  // ];
+  public helfer: Observable<Helfer[]>;
+
+  public constructor(private helferService: HelferService) {
+    this.helfer = helferService.getProducts();
+  }
 
   ngOnInit(): void {
   }
--- src/app/services/helfer.service.ts
+++ src/app/services/helfer.service.ts
@@ -1,15 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Helfer } from '../models/helfer.models';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class HelferService {
   constructor(private httpClient: HttpClient) { }
 
   public getProducts(): Observable<Helfer[]> {
-    return this.httpClient.get<Helfer[]>('/api/helfer');
+    // return this.httpClient.get<Helfer[]>('/api/helfer');
+    return of([
+      {
+        id: 1,
+        name: "name",
+        adress: "lange straße 1",
+        phonenumber: "123457"
+      },
+      {
+        id: 2,
+        name: "name",
+        adress: "lange straße 1",
+        phonenumber: "123457"
+      }
+    ]);
   }
 }
